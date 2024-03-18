43d6df0d15697fe921e7c52f4ea2e9e379613128
s27046 <s27046@pjwstk.edu.pl>
2024-03-19 00:03:29 +0100

    1

--- InitialProject/Program.cs
+++ InitialProject/Program.cs
@@ -1,7 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-lllllll
-jhvhgcgfx
-
-
-ffgggggg
+public static double CalculateAverage(int[] numbers)
+{
+    if (numbers.Length == 0)
+    {
+        return 0;
+    }
+    int sum = 0;
+    foreach (int num in numbers)
+    {
+        sum += num;
+    }
+    return (double)sum / numbers.Length;
+}
